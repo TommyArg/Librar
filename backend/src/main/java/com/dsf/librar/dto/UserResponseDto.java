a9a6f82172bc5c548b43de024d6a7f6e85891a1d
Kevin <kfornier@gmail.com>
2026-09-19 17:55:34 -0300

    feat(dto): add user request & response dto

---
+++ backend/src/main/java/com/dsf/librar/dto/UserResponseDto.java
@@ -0,0 +1,17 @@
+package com.dsf.librar.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String username;
+    private String completeName;
+    private Long role;
+    private Long sucursalId;
+    private Boolean active;
+}
